f0e349cb0279e77bd2e99be5bea0116e1af37dc0
aburakatay-jpg <aburakatay@gmail.com>
2026-08-17 14:21:03 +0300

    Change import from analyze_airdrop to analyze_opportunities

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import time
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from scripts.fetch_defillama import get_new_candidates
-from scripts.groq_analyze import analyze_airdrop
+from scripts.groq_analyze import analyze_opportunities
 from scripts.notify import send_message
 
 def main():
